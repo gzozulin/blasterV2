afba537b3f1901820d40ab1c520b26f27ff899bd
Grigory Zozulin <Grigory.Zozulin@soti.net>
2020-07-24 19:41:35 -0400

    stupid gradle fix

--- minigl/src/main/kotlin/com/gzozulin/minigl/assets/AssetStream.kt
+++ minigl/src/main/kotlin/com/gzozulin/minigl/assets/AssetStream.kt
@@ -2,7 +2,9 @@ package com.gzozulin.minigl.assets
 
 import java.io.File
 
-private val assets = File("assets")
+private val siblingDir = File("assets")
+private val parentDir = File("../assets")
+private val assets = if (siblingDir.exists()) siblingDir else parentDir
 
 class AssetStream {
     fun openAsset(filename: String) = File(assets, filename).inputStream()
