9286568047d545bcd19f76af46e3affd4a743b8b
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-01-23 15:10:37 -0800

    resizing

--- minigl/src/main/kotlin/com/gzozulin/minigl/assembly/SimpleText.kt
+++ minigl/src/main/kotlin/com/gzozulin/minigl/assembly/SimpleText.kt
@@ -18,7 +18,7 @@ data class TextSpan(val text: String, val color: col3 = col3().blue())
 data class TextPage(val spans: List<TextSpan>)
 
 class SimpleTextTechnique(
-    private val windowWidth: Int, private val windowHeight: Int) : GlResource() {
+    private var windowWidth: Int, private var windowHeight: Int) : GlResource() {
 
     private val rect = GlMesh.rect(0f, LETTER_SIZE_U, 0f, LETTER_SIZE_V)
     private val font = texturesLib.loadTexture("textures/font.png")
@@ -30,8 +30,13 @@ class SimpleTextTechnique(
     private val cursor = mat4().identity()
 
     private val modelM = unifm4(cursor)
-    private val projectionM = constm4(mat4().ortho(0f, windowWidth.toFloat(), 0f, windowHeight.toFloat(), -1f, 1f))
-    private val centerM = constm4(mat4().identity().translate(vec3(windowWidth / 2f, windowHeight / 2f, 0f)))
+
+    private val unifProj = unifm4(mat4().identity())
+    private val unifCenter = unifm4(mat4().identity())
+
+    init {
+        resize(windowWidth, windowHeight)
+    }
 
     private val tileUV = vec2i(0)
     private val unifTileUV = unifv2i(tileUV)
@@ -41,10 +46,17 @@ class SimpleTextTechnique(
     private val fontCheck = eq(tex(texCoordTiled, unifsampler(font)), constv4(vec4(1f)))
     private val result = ifexp(fontCheck, uniformColor, discard())
 
-    private val simpleTechnique = SimpleTechnique(modelM, centerM, projectionM, result)
+    private val simpleTechnique = SimpleTechnique(modelM, unifCenter, unifProj, result)
 
     init {
         addChildren(simpleTechnique, rect, font)
+    }
+
+    fun resize(width: Int, height: Int) {
+        windowWidth = width
+        windowHeight = height
+        unifProj.value!!.set(mat4().ortho(0f, windowWidth.toFloat(), 0f, windowHeight.toFloat(), -1f, 1f))
+        unifCenter.value!!.set(mat4().identity().translate(vec3(windowWidth / 2f, windowHeight / 2f, 0f)))
     }
 
     private fun updateCursor() {
@@ -135,6 +147,9 @@ private val simpleTextTechnique = SimpleTextTechnique(window.width, window.heigh
 
 fun main() {
     window.create(isHoldingCursor = false) {
+        window.resizeCallback = { width, height ->
+            simpleTextTechnique.resize(width, height)
+        }
         glUse(simpleTextTechnique) {
             window.show {
                 glClear(col3().ltGrey())
