54441af7272ace802b3af49c4681f385a4bedd97
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-02-11 14:59:13 -0800

    proper paging

--- sfcs/src/main/kotlin/com/gzozulin/proj/ProjApp.kt
+++ sfcs/src/main/kotlin/com/gzozulin/proj/ProjApp.kt
@@ -9,15 +9,15 @@ import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.Token
 import java.nio.ByteBuffer
+import kotlin.streams.toList
 
-private const val FRAMES_PER_PAGE = 10
+private const val FRAMES_PER_PAGE = 60
 
 private typealias DeclCtx = KotlinParser.DeclarationContext
 
-private data class ProjectorNode(val identifier: String, var isKnown: Boolean = false,
-                                 val children: List<ProjectorNode>? = null)
+private data class ProjectorNode(val order: Int, val identifier: String, val children: List<ProjectorNode>? = null)
 
-private data class RenderStep(val tokens: List<Token>)
+private data class RenderedToken(val order: Int, val token: Token)
 
 private val chars by lazy { CharStreams.fromFileName(
     "/home/greg/blaster/sfcs/src/main/kotlin/com/gzozulin/proj/UtilityClass.kt") }
@@ -25,14 +25,22 @@ private val lexer by lazy { KotlinLexer(chars) }
 private val tokens by lazy { CommonTokenStream(lexer) }
 private val parser by lazy { KotlinParser(tokens) }
 
+// todo: add the aility to "split" the node: i.e. MainClass first, then UtilityClass, then MainClass again
+private var nodeOrder = 0
 private val scenario = listOf(
-    ProjectorNode("MainClass", children = listOf(
-        ProjectorNode("originFunction"),
-        ProjectorNode("internalFlag"),
-        ProjectorNode("internalValue")
-    )))
+    ProjectorNode(
+        nodeOrder++,"MainClass", children = listOf(
+            ProjectorNode(nodeOrder++, "originFunction"),
+            ProjectorNode(nodeOrder++, "internalValue"),
+            ProjectorNode(nodeOrder++, "internalFlag"),
+    )),
+    ProjectorNode(nodeOrder++, "UtilityClass", children = listOf(
+        ProjectorNode(nodeOrder++, "internalFunction")
+    )),
+    ProjectorNode(nodeOrder++, "highlevelFunction")
+)
 
-private val renderedSteps = mutableListOf<RenderStep>()
+private val renderedTokens = mutableListOf<RenderedToken>()
 private val renderedPages = mutableListOf<TextPage>()
 
 private val capturer = GlCapturer()
@@ -43,7 +51,7 @@ private var currentPage = 0
 private var currentFrame = 0
 
 fun main() {
-    stepThrough(scenario)
+    renderScenario()
     preparePages()
     capturer.create {
         glUse(simpleTextTechnique) {
@@ -52,38 +60,31 @@ fun main() {
     }
 }
 
-private fun stepThrough(nodes: List<ProjectorNode>) {
-    nodes.forEach { node ->
-        node.isKnown = true
-        renderAll()
-        if (node.children != null) {
-            stepThrough(node.children)
+private fun renderScenario() {
+    parser.reset()
+    val visitor = visit(scenario) { increment ->
+        for (renderedToken in increment) {
+            if (!renderedTokens.contains(renderedToken)) {
+                renderedTokens += renderedToken
+            }
         }
     }
-}
-
-private fun renderAll() {
-    parser.reset()
-    val tokens = mutableListOf<Token>()
-    val visitor = visit(scenario) { increment ->
-        tokens += increment
-    }
     visitor.visitKotlinFile(parser.kotlinFile())
-    renderedSteps.add(RenderStep(tokens))
 }
 
-private fun visit(nodes: List<ProjectorNode>, result: (increment: List<Token>) -> Unit) =
+private fun visit(nodes: List<ProjectorNode>, result: (increment: List<RenderedToken>) -> Unit) =
     object : KotlinParserBaseVisitor<Unit>() {
         override fun visitDeclaration(decl: DeclCtx) {
             val identifier = decl.identifier()
-            val node = nodes.firstOrNull { it.identifier == identifier } ?: return
-            if (node.isKnown) {
-                if (node.children != null) {
-                    result.invoke(decl.predeclare())
-                    decl.visitNext(node.children, result)
-                    result.invoke(decl.postdeclare())
-                } else {
-                    result.invoke(decl.define())
+            for (node in nodes) {
+                if (node.identifier == identifier) {
+                    if (node.children != null) {
+                        result.invoke(decl.predeclare().withOrder(node.order))
+                        decl.visitNext(node.children, result)
+                        result.invoke(decl.postdeclare().withOrder(node.order))
+                    } else {
+                        result.invoke(decl.define().withOrder(node.order))
+                    }
                 }
             }
         }
@@ -114,7 +115,7 @@ private fun DeclCtx.postdeclare(): List<Token> {
     return tokens.get(start, stop.tokenIndex)
 }
 
-private fun DeclCtx.visitNext(nodes: List<ProjectorNode>, result: (increment: List<Token>) -> Unit) {
+private fun DeclCtx.visitNext(nodes: List<ProjectorNode>, result: (increment: List<RenderedToken>) -> Unit) {
     val visitor = visit(nodes, result)
     when {
         classDeclaration() != null -> visitor.visitClassDeclaration(classDeclaration())
@@ -125,6 +126,8 @@ private fun DeclCtx.visitNext(nodes: List<ProjectorNode>, result: (increment: Li
     }
 }
 
+private fun List<Token>.withOrder(step: Int) = stream().map { RenderedToken(step, it) }.toList()
+
 private fun Int.leftWS(): Int {
     var result = this - 1
     while (result >= 0 && tokens.get(result).type == KotlinLexer.WS) {
@@ -134,16 +137,14 @@ private fun Int.leftWS(): Int {
 }
 
 private fun preparePages() {
-    for (renderedStep in renderedSteps) {
-        val currentTokens = mutableListOf<Token>()
-        for (token in renderedStep.tokens) {
-            currentTokens.add(token)
-            val spans = mutableListOf<TextSpan>()
-            for (currentToken in currentTokens) {
-                spans.add(TextSpan(currentToken.text, col3().cyan()))
+    for (current in 0 until nodeOrder) {
+        val spans = mutableListOf<TextSpan>()
+        for (renderedToken in renderedTokens) {
+            if (renderedToken.order <= current) {
+                spans.add(TextSpan(renderedToken.token.text, col3().cyan()))
             }
-            renderedPages.add(TextPage(spans))
         }
+        renderedPages.add(TextPage(spans))
     }
 }
 
