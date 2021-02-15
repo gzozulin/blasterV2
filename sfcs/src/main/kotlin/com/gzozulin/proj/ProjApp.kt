e36ff18cb79e3adff95ec23e479a9ffe9b9e2c3b
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-02-15 14:15:56 -0800

    timings

--- sfcs/src/main/kotlin/com/gzozulin/proj/ProjApp.kt
+++ sfcs/src/main/kotlin/com/gzozulin/proj/ProjApp.kt
@@ -10,13 +10,16 @@ import org.antlr.v4.runtime.CommonToken
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.Token
 import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
 import kotlin.streams.toList
 
 private const val FRAMES_PER_SPAN = 5
+private const val MILLIS_PER_FRAME = 16
 
 private typealias DeclCtx = KotlinParser.DeclarationContext
 
-private data class ProjectorNode(val order: Int, val identifier: String, val children: List<ProjectorNode>? = null)
+private data class ProjectorNode(val order: Int, val timeout: Long,
+                                 val identifier: String, val children: List<ProjectorNode>? = null)
 
 data class OrderedToken(val order: Int, val token: Token)
 data class OrderedSpan(val order: Int, override val text: String, override val color: col3,
@@ -29,14 +32,15 @@ private val tokens by lazy { CommonTokenStream(lexer) }
 private val parser by lazy { KotlinParser(tokens) }
 
 // todo: add the aility to "split" the node: i.e. MainClass first, then UtilityClass, then MainClass again
-private var orderCnt = 0
+private var scenarioNodeCnt = 0
 private val scenario = listOf(
-    ProjectorNode(orderCnt++, "main"),
-    ProjectorNode(orderCnt++, "renderScenario"),
-    ProjectorNode(orderCnt++, "preparePage"),
-    ProjectorNode(orderCnt++, "capturer"),
-    ProjectorNode(orderCnt++, "visit"),
-    ProjectorNode(orderCnt++, "updateCursor"),
+    ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(7),"main"),
+    ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(2),"renderScenario"),
+    ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(2),"preparePage"),
+    ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(2),"capturer"),
+    ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(2),"Visitor",
+        children = listOf(ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(2),"visitDeclaration"))),
+    ProjectorNode(scenarioNodeCnt++, TimeUnit.SECONDS.toMillis(2),"updateCursor"),
 )
 
 private val orderedTokens = mutableListOf<OrderedToken>()
@@ -46,12 +50,15 @@ private val capturer = GlCapturer()
 
 private val simpleTextTechnique = SimpleTextTechnique(capturer.width, capturer.height)
 
+private var isAdvancingSpans = true // spans or timeout
 private var currentFrame = 0
 private var currentOrder = 0
+private var currentTimeout = 0L
 
 fun main() {
     renderScenario()
     preparePage()
+    prepareOrder()
     capturer.create {
         glUse(simpleTextTechnique) {
             capturer.show(::onFrame, ::onBuffer)
@@ -61,7 +68,7 @@ fun main() {
 
 private fun renderScenario() {
     parser.reset()
-    val visitor = visit(scenario) { increment ->
+    val visitor = Visitor(scenario) { increment ->
         if (increment.last().token.type != KotlinLexer.NL) {
             orderedTokens += addTrailingNl(increment)
         } else {
@@ -74,23 +81,23 @@ private fun renderScenario() {
 private fun addTrailingNl(increment: List<OrderedToken>) =
     listOf(*increment.toTypedArray(), OrderedToken(increment.first().order, CommonToken(KotlinParser.NL, "\n")))
 
-private fun visit(nodes: List<ProjectorNode>, result: (increment: List<OrderedToken>) -> Unit) =
-    object : KotlinParserBaseVisitor<Unit>() {
-        override fun visitDeclaration(decl: DeclCtx) {
-            val identifier = decl.identifier()
-            for (node in nodes) {
-                if (node.identifier == identifier) {
-                    if (node.children != null) {
-                        result.invoke(decl.predeclare().withOrder(node.order))
-                        decl.visitNext(node.children, result)
-                        result.invoke(decl.postdeclare().withOrder(node.order))
-                    } else {
-                        result.invoke(decl.define().withOrder(node.order))
-                    }
+private class Visitor(val nodes: List<ProjectorNode>, val result: (increment: List<OrderedToken>) -> Unit)
+    : KotlinParserBaseVisitor<Unit>() {
+    override fun visitDeclaration(decl: DeclCtx) {
+        val identifier = decl.identifier()
+        for (node in nodes) {
+            if (node.identifier == identifier) {
+                if (node.children != null) {
+                    result.invoke(decl.predeclare().withOrder(node.order))
+                    decl.visitNext(node.children, result)
+                    result.invoke(decl.postdeclare().withOrder(node.order))
+                } else {
+                    result.invoke(decl.define().withOrder(node.order))
                 }
             }
         }
     }
+}
 
 private fun DeclCtx.identifier() = when {
     classDeclaration() != null -> classDeclaration().simpleIdentifier().text
@@ -101,6 +108,7 @@ private fun DeclCtx.identifier() = when {
     else -> error("Unknown declaration!")
 }
 
+// FIXME: 2021-02-12 objects and functions
 private fun DeclCtx.predeclare(): List<Token> {
     val start = start.tokenIndex.leftPadding()
     val classDecl = classDeclaration()!!
@@ -128,7 +136,7 @@ private fun Int.leftPadding(): Int {
 }
 
 private fun DeclCtx.visitNext(nodes: List<ProjectorNode>, result: (increment: List<OrderedToken>) -> Unit) {
-    val visitor = visit(nodes, result)
+    val visitor = Visitor(nodes, result)
     when {
         classDeclaration() != null -> visitor.visitClassDeclaration(classDeclaration())
         functionDeclaration() != null -> visitor.visitFunctionDeclaration(functionDeclaration())
@@ -150,6 +158,14 @@ private fun preparePage() {
 
 fun OrderedToken.toOrderedSpan() = OrderedSpan(order, token.text, token.color(), visibility = SpanVisibility.GONE)
 
+private fun prepareOrder() {
+    renderedPage.spans
+        .filter { it.order == currentOrder }
+        .filter { it.text.isBlank() }
+        .forEach { it.visibility = SpanVisibility.VISIBLE }
+    currentTimeout = scenario[currentOrder].timeout
+}
+
 private fun onFrame() {
     glClear(col3().ltGrey())
     updateSpans()
@@ -157,42 +173,54 @@ private fun onFrame() {
 }
 
 private fun updateSpans() {
+    currentTimeout -= MILLIS_PER_FRAME
+    if (isAdvancingSpans) {
+        advanceSpans()
+    } else {
+        advanceTimeout()
+    }
+}
+
+private fun advanceSpans() {
     currentFrame++
     if (currentFrame == FRAMES_PER_SPAN) {
         currentFrame = 0
-        var found = false
-        for (orderedSpan in renderedPage.spans) {
-            if (orderedSpan.order == currentOrder) {
-                if (orderedSpan.visibility == SpanVisibility.GONE) {
-                    orderedSpan.visibility = SpanVisibility.VISIBLE
-                    if (orderedSpan.text.isNotBlank()) {
-                        // non-WS counts
-                        found = true
-                        break
-                    }
+        val found = makeNextNonWsSpanVisible()
+        if (!found) {
+            isAdvancingSpans = false
+        }
+    }
+}
+
+private fun makeNextNonWsSpanVisible(): Boolean {
+    for (orderedSpan in renderedPage.spans) {
+        if (orderedSpan.order == currentOrder) {
+            if (orderedSpan.visibility == SpanVisibility.GONE) {
+                orderedSpan.visibility = SpanVisibility.VISIBLE
+                if (orderedSpan.text.isNotBlank()) {
+                    // non-WS counts
+                    return true
                 }
             }
         }
-        if (!found) {
-            nextOrder()
-            showWS()
-        }
+    }
+    return false
+}
+
+private fun advanceTimeout() {
+    if (currentTimeout <= 0) {
+        isAdvancingSpans = true
+        nextOrder()
+        prepareOrder()
     }
 }
 
 private fun nextOrder() {
     currentOrder++
-    if (currentOrder == orderCnt) {
+    if (currentOrder == scenarioNodeCnt - 1) {
         currentOrder = 0
         renderedPage.spans.forEach { it.visibility = SpanVisibility.GONE }
     }
-}
-
-private fun showWS() {
-    renderedPage.spans
-        .filter { it.order == currentOrder }
-        .filter { it.text.isBlank() }
-        .forEach { it.visibility = SpanVisibility.VISIBLE }
 }
 
 private fun onBuffer(buffer: ByteBuffer) {
