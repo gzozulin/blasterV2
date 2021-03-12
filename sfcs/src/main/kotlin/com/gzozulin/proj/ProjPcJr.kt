05eb741e4f685b90fc09e4f97bc6fdbcaccfe229
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-03-11 20:34:03 -0800

    dynamic

--- sfcs/src/main/kotlin/com/gzozulin/proj/ProjPcJr.kt
+++ sfcs/src/main/kotlin/com/gzozulin/proj/ProjPcJr.kt
@@ -1,8 +1,6 @@
 package com.gzozulin.proj
 
-import com.gzozulin.minigl.assembly.SimpleSpan
-import com.gzozulin.minigl.assembly.SimpleTextTechnique
-import com.gzozulin.minigl.assembly.TextPage
+import com.gzozulin.minigl.assembly.*
 import com.gzozulin.minigl.assets.meshLib
 import com.gzozulin.minigl.assets.texturesLib
 import com.gzozulin.minigl.gl.*
@@ -22,17 +20,31 @@ private val wasdInput = WasdInput(controller)
 
 private val skyboxTechnique = StaticSkyboxTechnique("textures/miramar")
 private val simpleTechnique = StaticSimpleTechnique()
-private val simpleTextTechnique = SimpleTextTechnique(windowWidth = window.width, windowHeight = window.height)
 
-private val examplePage = TextPage(listOf(
-    SimpleSpan("Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when\n" +
-            "an unknown printer took a galley of type and scrambled it to make a type specimen book.\n" +
-            "It has survived not only five centuries, but also the leap into electronic typesetting,\n" +
-            "remaining essentially unchanged. It was popularised in the 1960s with the release of\n" +
-            "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing\n" +
-            "software like Aldus PageMaker including versions of Lorem Ipsum.\n\n", color = col3().green())
-))
+private val fontDescription = FontDescription(
+    textureFilename = "textures/font_hires.png",
+    glyphSidePxU = 64, glyphSidePxV = 64,
+    fontScaleU = 0.2f, fontScaleV = 0.2f,
+    fontStepScaleU = 0.45f, fontStepScaleV = 0.75f)
+
+private val simpleTextTechnique = SimpleTextTechnique(fontDescription, window.width, window.height)
+
+private val text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when\n" +
+        "an unknown printer took a galley of type and scrambled it to make a type specimen book.\n" +
+        "It has survived not only five centuries, but also the leap into electronic typesetting,\n" +
+        "remaining essentially unchanged. It was popularised in the 1960s with the release of\n" +
+        "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing\n" +
+        "software like Aldus PageMaker including versions of Lorem Ipsum."
+
+private val spans = text.run {
+    val words = split(' ')
+    val spans = mutableListOf<SimpleSpan>()
+    words.forEach { spans.add(SimpleSpan("$it ", col3().green(), SpanVisibility.INVISIBLE)) }
+    return@run spans
+}
+
+private val examplePage = TextPage(spans)
 
 private val framebuffer = GlFrameBuffer()
 private var textTex = GlTexture(
@@ -46,10 +58,21 @@ private val pcjr = meshLib.loadMesh("models/pcjr/pcjr.obj") { println(it) }
 
 private var mouseLook = false
 
-private fun updateInput() {
+private var frame = 0
+
+private fun updateScene() {
+    frame++
     controller.apply { position, direction ->
         camera.setPosition(position)
         camera.lookAlong(direction)
+    }
+    if (frame % 3 == 0) {
+        val found = spans.firstOrNull { it.visibility == SpanVisibility.INVISIBLE }
+        if (found == null) {
+            spans.forEach { it.visibility = SpanVisibility.INVISIBLE }
+        } else {
+            found.visibility = SpanVisibility.VISIBLE
+        }
     }
 }
 
@@ -99,7 +122,7 @@ fun main() {
         glUse(simpleTechnique, skyboxTechnique, simpleTextTechnique,
             framebuffer, pcjr.mesh, pcjrtex, textRect, textTex) {
             window.show {
-                updateInput()
+                updateScene()
                 renderText()
                 renderScene()
             }
