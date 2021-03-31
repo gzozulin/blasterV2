cbd67447890ce350fdf4677d45c47732f2d5e439
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-03-30 18:43:31 -0700

    deferred bedroom

--- sfcs/src/main/kotlin/com/gzozulin/proj/BedroomMulti.kt
+++ sfcs/src/main/kotlin/com/gzozulin/proj/BedroomMulti.kt
@@ -2,10 +2,11 @@ package com.gzozulin.proj
 
 import com.gzozulin.minigl.api.*
 import com.gzozulin.minigl.assembly.*
+import com.gzozulin.minigl.assets.Object
 import com.gzozulin.minigl.assets.modelLib
-import com.gzozulin.minigl.scene.Camera
+import com.gzozulin.minigl.assets.texturesLib
+import com.gzozulin.minigl.scene.*
 import com.gzozulin.minigl.scene.Controller
-import com.gzozulin.minigl.scene.WasdInput
 import com.gzozulin.minigl.techniques.StaticSkyboxTechnique
 
 private val window = GlWindow()
@@ -16,20 +17,30 @@ private val wasdInput = WasdInput(controller)
 
 private val skyboxTechnique = StaticSkyboxTechnique("textures/snowy")
 
+private val modelM = constm4(mat4().identity())
+private val viewM = unifm4 { camera.calculateViewM() }
+private val eye = unifv3 { camera.position }
+private val projM = unifm4 { camera.projectionM }
+
 private val texCoords = varying<vec2>(SimpleVarrying.vTexCoord.name)
-private val unifSampler = unifsampler()
-private val unifColor = unifv4()
+private val sampler = unifsampler()
+private val diffuseMap = tex(texCoords, sampler)
+private val diffuseColor = constv4(vec4(1f))
 
-private val identityM = constm4(mat4().identity())
-private val unifViewM = unifm4(camera.calculateViewM())
-private val projM = constm4(camera.projectionM)
+private val matAmbient = unifv3()
+private val matDiffuse = unifv3()
+private val matSpecular = unifv3()
+private val matShine = uniff()
+private val matTransparency = uniff()
 
-private val flatTexTechnique = FlatTechnique(identityM, unifViewM, projM, tex(texCoords, unifSampler))
-private val flatColorTechnique = FlatTechnique(identityM, unifViewM, projM, unifColor)
+private val deferredTextureTechnique = DeferredTechnique(
+    modelM, viewM, projM, eye, diffuseMap, matAmbient, matDiffuse, matSpecular, matShine, matTransparency)
 
 private val model = modelLib.load("models/bedroom/bedroom", join = false)
-private val colorOnly = model.objects.filter { it.phong().mapDiffuse == null }
-private val texOnly = model.objects.filter { it.phong().mapDiffuse != null }
+private val empty = texturesLib.loadTexture("textures/snow.png")
+
+private val light = PointLight(model.aabb.center(), vec3(1f), 1000f)
+private val lights = listOf(light)
 
 private var mouseLook = false
 
@@ -48,7 +59,10 @@ fun main() {
         window.keyCallback = { key, pressed ->
             wasdInput.onKeyPressed(key, pressed)
         }
-        glUse(skyboxTechnique, model, flatTexTechnique, flatColorTechnique) {
+        window.resizeCallback = { width: Int, height: Int ->
+            deferredTextureTechnique.resize(width, height)
+        }
+        glUse(skyboxTechnique, deferredTextureTechnique, model, empty) {
             window.show {
                 glClear()
                 controller.apply { position, direction ->
@@ -56,19 +70,21 @@ fun main() {
                     camera.lookAlong(direction)
                 }
                 skyboxTechnique.skybox(camera)
-                unifViewM.value = camera.calculateViewM()
+                light.position.set(camera.position)
                 glMultiSample {
                     glDepthTest {
-                        flatColorTechnique.draw {
-                            colorOnly.forEach {
-                                unifColor.value = vec4(it.phong().diffuse, 1f)
-                                flatColorTechnique.instance(it)
-                            }
-                        }
-                        flatTexTechnique.draw {
-                            texOnly.forEach {
-                                unifSampler.value = it.phong().mapDiffuse!!
-                                flatTexTechnique.instance(it)
+                        glBind(empty) {
+                            deferredTextureTechnique.draw(lights) {
+                                model.objects.forEach { obj ->
+                                    val material = obj.phong()
+                                    sampler.value = material.mapDiffuse ?: empty
+                                    matAmbient.value = vec3(0f)
+                                    matDiffuse.value = material.diffuse
+                                    matSpecular.value = material.specular
+                                    matShine.value = material.shine
+                                    matTransparency.value = material.transparency
+                                    deferredTextureTechnique.instance(obj)
+                                }
                             }
                         }
                     }
