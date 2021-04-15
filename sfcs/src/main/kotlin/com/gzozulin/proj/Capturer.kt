741c232e369c6b4f36cc87bc82a2bd167f3f9a7d
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-04-14 19:17:29 -0700

    capturing

--- sfcs/src/main/kotlin/com/gzozulin/proj/Capturer.kt
+++ sfcs/src/main/kotlin/com/gzozulin/proj/Capturer.kt
@@ -1,10 +1,10 @@
 package com.gzozulin.proj
 
 import com.gzozulin.minigl.api.GlWindow
-import org.bytedeco.ffmpeg.global.avcodec
-import org.bytedeco.ffmpeg.global.avutil
+import org.bytedeco.ffmpeg.global.avcodec.AV_CODEC_ID_RAWVIDEO
+import org.bytedeco.ffmpeg.global.avutil.AV_PIX_FMT_RGBA
+import org.bytedeco.ffmpeg.global.avutil.AV_PIX_FMT_YUV420P
 import org.bytedeco.javacpp.BytePointer
-import org.bytedeco.javacv.DC1394FrameGrabber
 import org.bytedeco.javacv.FFmpegFrameRecorder
 import org.bytedeco.javacv.OpenCVFrameConverter
 import org.bytedeco.opencv.global.opencv_core.flip
@@ -18,8 +18,8 @@ class Capturer(window: GlWindow, private val width: Int, private val height: Int
     private val converter = OpenCVFrameConverter.ToMat()
 
     init {
-        recorder.videoCodec = avcodec.AV_CODEC_ID_RAWVIDEO
-        recorder.pixelFormat = avutil.AV_PIX_FMT_YUV420P
+        recorder.videoCodec = AV_CODEC_ID_RAWVIDEO
+        recorder.pixelFormat = AV_PIX_FMT_YUV420P
         recorder.frameRate = 60.0
     }
 
@@ -45,7 +45,7 @@ class Capturer(window: GlWindow, private val width: Int, private val height: Int
     fun onBuffer() {
         if (IS_CAPTURING) {
             flip(originalFrame, flippedFrame, 0) // vertical flip
-            recorder.record(frame)
+            recorder.record(frame, AV_PIX_FMT_RGBA)
         }
     }
 }
