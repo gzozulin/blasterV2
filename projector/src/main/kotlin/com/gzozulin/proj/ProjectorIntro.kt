14b08cdfd566d414697062d653e44c2a31ddc2c8
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-05-21 12:27:51 -0700

    dist check

--- projector/src/main/kotlin/com/gzozulin/proj/ProjectorIntro.kt
+++ projector/src/main/kotlin/com/gzozulin/proj/ProjectorIntro.kt
@@ -1,6 +1,7 @@
 package com.gzozulin.proj
 
 import com.gzozulin.minigl.api.*
+import com.gzozulin.minigl.assets.libTextureCreate
 import com.gzozulin.minigl.assets.libTextureCreatePbr
 import com.gzozulin.minigl.assets.libWavefrontCreate
 import com.gzozulin.minigl.assets.libWavefrontGroupUse
@@ -8,10 +9,11 @@ import com.gzozulin.minigl.scene.*
 import com.gzozulin.minigl.techniques.*
 
 private const val TEXTURE_SIDE = 4096
-private const val HORIZONTAL_CNT = 24
-private const val VERTICAL_CNT = 24
+private const val HORIZONTAL_CNT = 32
+private const val VERTICAL_CNT = 32
 private const val HORIZONTAL_STEP = 5f
 private const val VERTICAL_STEP = 3.5f
+private const val DRAW_DISTANCE = 100f
 
 private val window = GlWindow(isFullscreen = true, isHoldingCursor = false, isMultisampling = true)
 
@@ -30,9 +32,15 @@ private val itemLight = PointLight(vec3(3f), vec3(1f), 100f)
 
 private val teapotGroup = libWavefrontCreate("models/teapot/teapot")
 private val teapotObject = teapotGroup.objects.first()
+private val teapotTexture = libTextureCreate("textures/marble.jpg")
 
 private val computerGroup = libWavefrontCreate("models/pcjr/pcjr")
 private val computerObject = computerGroup.objects.first()
+private val computerTexture = computerObject.phong.mapDiffuse!!
+
+private val mandalorianGroup = libWavefrontCreate("models/mandalorian/mandalorian")
+private val mandalorianObject = mandalorianGroup.objects.first()
+private val mandalorianTexture = libTextureCreate("models/mandalorian/albedo.png")
 
 // -------------------------------- Lights --------------------------------
 
@@ -99,7 +107,7 @@ private val unifTvMetallic    = sampler(unifs(tvMaterial.metallic))
 private val unifTvRoughness   = sampler(unifs(tvMaterial.roughness))
 private val unifTvAO          = sampler(unifs(tvMaterial.ao))
 
-private enum class TvType { TEAPOT, COMPUTER }
+private enum class TvType { TEAPOT, COMPUTER, MANDALORIAN }
 private data class TvInstance(val type: TvType, val matrix: mat4)
 
 private val tvs = mutableListOf<TvInstance>().apply {
@@ -128,19 +136,26 @@ private fun tvsDraw() {
         glDepthTest {
             glTextureBind(computerRtt.color) {
                 glTextureBind(teapotRtt.color) {
-                    glTextureBind(tvMaterial.normal) {
-                        glTextureBind(tvMaterial.metallic) {
-                            glTextureBind(tvMaterial.roughness) {
-                                glTextureBind(tvMaterial.ao) {
-                                    lightCamera.position.set(camera.position).add(0f, 0f, -1f)
-                                    glShadingPbrDraw(shadingPbr, lightsAll) {
-                                        tvs.forEach {
-                                            unifTvModel.value = it.matrix
-                                            when (it.type) {
-                                                TvType.TEAPOT -> unifTvItemAlbedo.value = teapotRtt.color
-                                                TvType.COMPUTER -> unifTvItemAlbedo.value = computerRtt.color
+                    glTextureBind(mandalorianRtt.color) {
+                        glTextureBind(tvMaterial.normal) {
+                            glTextureBind(tvMaterial.metallic) {
+                                glTextureBind(tvMaterial.roughness) {
+                                    glTextureBind(tvMaterial.ao) {
+                                        lightCamera.position.set(camera.position).add(0f, 0f, -1f)
+                                        glShadingPbrDraw(shadingPbr, lightsAll) {
+                                            for (tv in tvs) {
+                                                val worldPosition = tv.matrix.transform(vec4())
+                                                if (worldPosition.distance(vec4(camera.position, 1f)) > DRAW_DISTANCE) {
+                                                    continue
+                                                }
+                                                unifTvModel.value = tv.matrix
+                                                when (tv.type) {
+                                                    TvType.TEAPOT -> unifTvItemAlbedo.value = teapotRtt.color
+                                                    TvType.COMPUTER -> unifTvItemAlbedo.value = computerRtt.color
+                                                    TvType.MANDALORIAN -> unifTvItemAlbedo.value = mandalorianRtt.color
+                                                }
+                                                glShadingPbrInstance(shadingPbr, tvObject.mesh)
                                             }
-                                            glShadingPbrInstance(shadingPbr, tvObject.mesh)
                                         }
                                     }
                                 }
@@ -161,9 +176,13 @@ private val teapotMatrix = mat4()
 private var computerRotation = 0f
 private val computerMatrix = mat4()
 
-private val unifItemModel = unifm4()
-private val unifItemAlbedo = unifv4()
+private var mandalorianRotation = 0f
+private val mandalorianMatrix = mat4()
 
+private val unifItemModel = unifm4()
+private val unifItemAlbedo = unifs()
+
+private val unifItemAlbedoSampler = sampler(unifItemAlbedo)
 private val constItemView = constm4(mat4().identity())
 private val constItemProj = constm4(camera.projectionM)
 private val constIteEye = constv3(vec3().zero())
@@ -183,15 +202,24 @@ private val mergingItem = TechniquePostProcessing(TEXTURE_SIDE, TEXTURE_SIDE, ::
 
 private val teapotRtt = TechniqueRtt(TEXTURE_SIDE, TEXTURE_SIDE)
 private val computerRtt = TechniqueRtt(TEXTURE_SIDE, TEXTURE_SIDE)
+private val mandalorianRtt = TechniqueRtt(TEXTURE_SIDE, TEXTURE_SIDE)
 
 private fun itemUse(callback: Callback) {
     glTechRttUse(teapotRtt) {
         glTechRttUse(computerRtt) {
-            glTechPostProcessingUse(mergingItem) {
-                glShadingPhongUse(shadingPhong) {
-                    libWavefrontGroupUse(teapotGroup) {
-                        libWavefrontGroupUse(computerGroup) {
-                            callback.invoke()
+            glTechRttUse(mandalorianRtt) {
+                glTechPostProcessingUse(mergingItem) {
+                    glShadingPhongUse(shadingPhong) {
+                        glTextureUse(teapotTexture) {
+                            libWavefrontGroupUse(mandalorianGroup) {
+                                glTextureUse(mandalorianTexture) {
+                                    libWavefrontGroupUse(teapotGroup) {
+                                        libWavefrontGroupUse(computerGroup) {
+                                            callback.invoke()
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -214,20 +242,31 @@ private fun computerUpdate() {
         .translate(-10f, 2f, -10f)
         .rotate(radf(-90f), vec3().front())
         .rotate(computerRotation, vec3().up())
-        .scale(5f)
+        .scale(6f)
 }
 
-private fun itemDraw(where: TechniqueRtt, mesh: GlMesh, matrix: mat4, albedo: vec4) {
+private fun mandalorianUpdate() {
+    mandalorianRotation += 0.01f
+    mandalorianMatrix.identity()
+        .translate(-10f, 1.8f, -9f)
+        .rotate(radf(-90f), vec3().front())
+        .rotate(mandalorianRotation, vec3().up())
+        .scale(0.15f)
+}
+
+private fun itemDraw(where: TechniqueRtt, mesh: GlMesh, matrix: mat4, albedo: GlTexture) {
     unifItemModel.value = matrix
     unifItemAlbedo.value = albedo
     glTechRttDraw(where) {
         glTextureBind(tvMaterial.albedo) {
-            glTechPostProcessingDraw(mergingItem) {
-                glDepthTest {
-                    glCulling {
-                        glClear()
-                        glShadingPhongDraw(shadingPhong, listOf(itemLight)) {
-                            glShadingPhongInstance(shadingPhong, mesh)
+            glTextureBind(albedo) {
+                glTechPostProcessingDraw(mergingItem) {
+                    glDepthTest {
+                        glCulling {
+                            glClear()
+                            glShadingPhongDraw(shadingPhong, listOf(itemLight)) {
+                                glShadingPhongInstance(shadingPhong, mesh)
+                            }
                         }
                     }
                 }
@@ -239,7 +278,7 @@ private fun itemDraw(where: TechniqueRtt, mesh: GlMesh, matrix: mat4, albedo: ve
 // -------------------------------- Techniques --------------------------------
 
 private val shadingPhong = ShadingPhong(unifItemModel, constItemView, constItemProj, constIteEye,
-    unifItemAlbedo, constItemMatAmbient, constItemMatDiffuse, constItemMatSpecular, constItemMatShine)
+    unifItemAlbedoSampler, constItemMatAmbient, constItemMatDiffuse, constItemMatSpecular, constItemMatShine)
 
 private val unifTvItemAlbedo = unifs(computerRtt.color)
 private val unifTvItemAlbedoSampler = sampler(unifTvItemAlbedo)
@@ -275,13 +314,15 @@ fun main() {
                     lightsUpdate()
                     teapotUpdate()
                     computerUpdate()
+                    mandalorianUpdate()
                     glClear(col3().black())
                     controller.apply { position, direction ->
                         camera.setPosition(position)
                         camera.lookAlong(direction)
                     }
-                    itemDraw(teapotRtt, teapotObject.mesh, teapotMatrix, vec4(col3().red(), 1f))
-                    itemDraw(computerRtt, computerObject.mesh, computerMatrix, vec4(col3().blue(), 1f))
+                    itemDraw(teapotRtt, teapotObject.mesh, teapotMatrix, teapotTexture)
+                    itemDraw(computerRtt, computerObject.mesh, computerMatrix, computerTexture)
+                    itemDraw(mandalorianRtt, mandalorianObject.mesh, mandalorianMatrix, mandalorianTexture)
                     tvsDraw()
                 }
             }
