eaafde82714e8b75b4bf50c811ef21a81dc861c4
Grigory Zozulin <GrigoryZozulin@vtech.ca>
2021-06-08 17:43:45 -0700

    c shaders

--- shaderlang/main.c
+++ shaderlang/main.c
@@ -158,6 +158,11 @@ struct mat3 m3ident() {
 // ------------------- BOOL -------------------
 
 public
+bool eqv2(const struct vec2 left, const struct vec2 right) {
+    return left.x == right.x && left.y == right.y;
+}
+
+public
 bool eqv3(const struct vec3 left, const struct vec3 right) {
     return left.x == right.x && left.y == right.y && left.z == right.z;
 }
@@ -526,8 +531,7 @@ struct vec4 shadingPbr(const struct vec3 albedo, const struct vec3 N, const floa
         float denominator = 4.0f * max(dotv3(N, V), 0.0f) * max(dotv3(N, L), 0.0f) + 0.001f;
 
         struct vec3 specular = divv3f(nominator, denominator);
-        struct vec3 kS = F;
-        struct vec3 kD = subv3(ftov3(1.0f), kS);
+        struct vec3 kD = subv3(ftov3(1.0f), F);
 
         kD = mulv3(kD, ftov3(1.0f - metallic));
 
@@ -546,49 +550,34 @@ struct vec4 shadingPbr(const struct vec3 albedo, const struct vec3 N, const floa
 // ------------------- MAIN ---------------
 
 int main() {
-    {
-        assert(eqv3(v3(1, 1, 1), v3(1, 1, 1)));
-        assert(!eqv3(v3(1, 1, 1), v3(1, 0, 1)));
-    }
-    {
-        assert(eqv3(negv3(ftov3(1)), ftov3(-1)));
-        assert(dotv3(v3(1, 0, 0), v3(0, 1, 0)) == 0.0);
-        assert(dotv3(v3(1, 0, 0), v3(1, 0, 0)) == 1.0);
-        assert(eqv3(crossv3(v3(1, 0, 0), v3(0, 1, 0)), v3(0, 0, 1)));
-        assert(eqv3(addv3(v3(1, 1, 1), v3(2, 2, 2)), v3(3, 3, 3)));
-        assert(eqv3(subv3(v3(1, 1, 1), v3(2, 2, 2)), v3(-1, -1, -1)));
-        assert(eqv3(mulv3(v3(1, 1, 1), v3(2, 2, 2)), v3(2, 2, 2)));
-        assert(eqv3(mulv3f(v3(1, 1, 1), 2.5f), v3(2.5f, 2.5f, 2.5f)));
-        assert(eqv3(powv3(ftov3(2.0f), ftov3(2.0f)), ftov3(4.0f)));
-        assert(eqv3(divv3f(v3(10, 10, 10), 5.0f), v3(2.0f, 2.0f, 2.0f)));
-        assert(eqv3(divv3(ftov3(4.0f), ftov3(2.0f)), ftov3(2.0f)));
-        assert(eqv3(mixv3(ftov3(1.0f), ftov3(1.0f), 0.5f), ftov3(1.0f)));
-        assert(eqv4(addv4(v4(1, 1, 1, 1), v4(2, 2, 2, 2)), v4(3, 3, 3, 3)));
-        assert(eqv4(subv4(v4(1, 1, 1, 1), v4(2, 2, 2, 2)), v4(-1, -1, -1, -1)));
-        assert(eqv4(mulv4(v4(1, 1, 1, 1), v4(2, 2, 2, 2)), v4(2, 2, 2, 2)));
-        assert(eqv4(mulv4f(v4(1, 1, 1, 1), 2.5f), v4(2.5f, 2.5f, 2.5f, 2.5f)));
-        assert(eqv4(divv4(ftov4(4.0f), ftov4(2.0f)), ftov4(2.0f)));
-        assert(eqv4(divv4f(v4(10, 10, 10, 10), 5.0f), v4(2.0f, 2.0f, 2.0f, 2.0f)));
-    }
-    {
-        assert(itof(123) == 123.0f);
-        assert(ftoi(123.5f) == 123);
-    }
-    {
-        struct vec2 t = tile(v2(1.0f, 1.0f), iv2(1, 1), iv2(2, 2));
-        assert(t.x == 1.0f);
-        assert(t.y == 1.0f);
-    }
-    {
-        assert(lenv3(v3(0, 0, 0)) == 0);
-        assert(lenv3(v3(1, 0, 0)) == 1);
-        assert(lenv3(v3(0, 1, 0)) == 1);
-        assert(lenv3(v3(0, 0, 1)) == 1);
-    }
-    {
-        struct vec3 norm = normv3(v3(10, 10, 10));
-        assert(lenv3(norm) - 1.0f < FLT_EPSILON);
-    }
+    assert(eqv3(v3(1, 1, 1), v3(1, 1, 1)));
+    assert(!eqv3(v3(1, 1, 1), v3(1, 0, 1)));
+    assert(eqv3(negv3(ftov3(1)), ftov3(-1)));
+    assert(dotv3(v3(1, 0, 0), v3(0, 1, 0)) == 0.0);
+    assert(dotv3(v3(1, 0, 0), v3(1, 0, 0)) == 1.0);
+    assert(eqv3(crossv3(v3(1, 0, 0), v3(0, 1, 0)), v3(0, 0, 1)));
+    assert(eqv3(addv3(v3(1, 1, 1), v3(2, 2, 2)), v3(3, 3, 3)));
+    assert(eqv3(subv3(v3(1, 1, 1), v3(2, 2, 2)), v3(-1, -1, -1)));
+    assert(eqv3(mulv3(v3(1, 1, 1), v3(2, 2, 2)), v3(2, 2, 2)));
+    assert(eqv3(mulv3f(v3(1, 1, 1), 2.5f), v3(2.5f, 2.5f, 2.5f)));
+    assert(eqv3(powv3(ftov3(2.0f), ftov3(2.0f)), ftov3(4.0f)));
+    assert(eqv3(divv3f(v3(10, 10, 10), 5.0f), v3(2.0f, 2.0f, 2.0f)));
+    assert(eqv3(divv3(ftov3(4.0f), ftov3(2.0f)), ftov3(2.0f)));
+    assert(eqv3(mixv3(ftov3(1.0f), ftov3(1.0f), 0.5f), ftov3(1.0f)));
+    assert(eqv4(addv4(v4(1, 1, 1, 1), v4(2, 2, 2, 2)), v4(3, 3, 3, 3)));
+    assert(eqv4(subv4(v4(1, 1, 1, 1), v4(2, 2, 2, 2)), v4(-1, -1, -1, -1)));
+    assert(eqv4(mulv4(v4(1, 1, 1, 1), v4(2, 2, 2, 2)), v4(2, 2, 2, 2)));
+    assert(eqv4(mulv4f(v4(1, 1, 1, 1), 2.5f), v4(2.5f, 2.5f, 2.5f, 2.5f)));
+    assert(eqv4(divv4(ftov4(4.0f), ftov4(2.0f)), ftov4(2.0f)));
+    assert(eqv4(divv4f(v4(10, 10, 10, 10), 5.0f), v4(2.0f, 2.0f, 2.0f, 2.0f)));
+    assert(itof(123) == 123.0f);
+    assert(ftoi(123.5f) == 123);
+    assert(eqv2(tile(v2(1.0f, 1.0f), iv2(1, 1), iv2(2, 2)), ftov2(1.0f)));
+    assert(lenv3(v3(0, 0, 0)) == 0);
+    assert(lenv3(v3(1, 0, 0)) == 1);
+    assert(lenv3(v3(0, 1, 0)) == 1);
+    assert(lenv3(v3(0, 0, 1)) == 1);
+    assert(lenv3(normv3(v3(10, 10, 10))) - 1.0f < FLT_EPSILON);
     return 0;
 }
 
